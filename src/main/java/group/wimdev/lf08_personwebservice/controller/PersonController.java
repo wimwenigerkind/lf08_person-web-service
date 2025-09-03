a8a789017c4fb3838d5d51b2099c76d440c8bd24
Wim Wenigerkind <wim.wenigerkind@heptacom.de>
2025-09-03 09:19:30 +0200

    Add createPerson method to PersonController for handling POST requests

--- src/main/java/group/wimdev/lf08_personwebservice/controller/PersonController.java
+++ src/main/java/group/wimdev/lf08_personwebservice/controller/PersonController.java
@@ -1,11 +1,26 @@
 package group.wimdev.lf08_personwebservice.controller;
 
+import group.wimdev.lf08_personwebservice.model.Person;
 import group.wimdev.lf08_personwebservice.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PersonController {
     @Autowired
     private PersonRepository personRepository;
+
+        @RequestMapping(value = "/SpringBootCrudService/person",
+        method = RequestMethod.POST,
+        consumes = {MediaType.APPLICATION_JSON_VALUE,
+            MediaType.APPLICATION_XML_VALUE},
+        produces = {MediaType.APPLICATION_JSON_VALUE,
+            MediaType.APPLICATION_XML_VALUE})
+    public Person createPerson(@RequestBody Person newPerson) {
+        return personRepository.save(newPerson);
+    }
 }
