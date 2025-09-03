a6fdfc6bad5b0ec4b0f66bf9000cabb184e7cbab
Wim Wenigerkind <wim.wenigerkind@heptacom.de>
2025-09-03 09:22:33 +0200

    Add getPersonById method to PersonController for retrieving a person by ID

--- src/main/java/group/wimdev/lf08_personwebservice/controller/PersonController.java
+++ src/main/java/group/wimdev/lf08_personwebservice/controller/PersonController.java
@@ -4,10 +4,9 @@ import group.wimdev.lf08_personwebservice.model.Person;
 import group.wimdev.lf08_personwebservice.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 public class PersonController {
@@ -22,5 +21,18 @@ public class PersonController {
             MediaType.APPLICATION_XML_VALUE})
     public Person createPerson(@RequestBody Person newPerson) {
         return personRepository.save(newPerson);
+    }
+
+    @RequestMapping(value = "/SpringBootCrudService/person/{id}",
+        method = RequestMethod.GET,
+        produces = {MediaType.APPLICATION_JSON_VALUE,
+            MediaType.APPLICATION_XML_VALUE})
+    public Person getPersonById(@PathVariable long id) {
+        Optional<Person> person = this.personRepository.findById(id);
+        if (person.isPresent()) {
+            return person.get();
+        } else {
+            throw new RuntimeException("Could not find person " + id);
+        }
     }
 }
