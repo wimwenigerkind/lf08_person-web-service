d32e4503b2d8aeefd2a7e5bbf5628b21a201ee16
Wim Wenigerkind <wim.wenigerkind@heptacom.de>
2025-09-03 09:09:40 +0200

    Add Person entity class with JPA annotations

---
+++ src/main/java/group/wimdev/lf08_personwebservice/model/Person.java
@@ -0,0 +1,39 @@
+package group.wimdev.lf08_personwebservice.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstname;
+    private String lastname;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+}
